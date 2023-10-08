4b809e8030eb41a295417c7e727f403802e533fa
abdullahi <jan.ab1517@gmail.com>
2023-10-08 08:54:04 +0100

    create api view and get jason response output

--- crud/api/models.py
+++ crud/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Crud(models.Model):
+    title = models.CharField(max_length=150)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
--- crud/api/views.py
+++ crud/api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def apiOverview(request):
+    return JsonResponse("heya this is new api", safe=False)
