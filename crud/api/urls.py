4ba5e8069a1a8e1f3e6e8cbf75254d280713579a
abdullahi <jan.ab1517@gmail.com>
2023-10-08 10:37:03 +0100

    add task detail and registered to admin page

--- crud/api/urls.py
+++ crud/api/urls.py
@@ -4,4 +4,5 @@ from api import views
 urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
     path('task-list/', views.crudList, name="task-list"),
+    path('task-detail/<str:pk>/', views.crudDetail, name="task-detail"),
 ]
